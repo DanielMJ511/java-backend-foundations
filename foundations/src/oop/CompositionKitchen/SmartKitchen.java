7f4954aea987ed2008aeb6d57ea2ade71b28e5ce
Daniel Mejia <bludani5@gmail.com>
2026-04-22 10:46:44 -0500

    Add SmartKitchen composition example
    Add SmartKitchen demonstrating object composition: a SmartKitchen holds a CoffeeMaker, Refrigerator, and DishWasher. Provides setKitchenState(...) to mark work for each component, doKitchenWork() to trigger component actions, and getters for each device. Also includes simple implementations of CoffeeMaker, Refrigerator, and DishWasher that print status messages. File added at foundations/src/oop/CompositionKitchen/SmartKitchen.java.

---
+++ foundations/src/oop/CompositionKitchen/SmartKitchen.java
@@ -0,0 +1,97 @@
+package oop.CompositionKitchen;
+
+public class SmartKitchen {
+    private CoffeeMaker brewMaster;
+    private Refrigerator iceBox;
+    private DishWasher dishWasher;
+
+    public SmartKitchen() {
+        brewMaster = new CoffeeMaker();
+        iceBox = new Refrigerator();
+        dishWasher = new DishWasher();
+    }
+
+    public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag,
+                                Boolean dishwasherFlag) {
+        brewMaster.setHasWorkToDo(coffeeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishWasher.setHasWorkToDo(dishwasherFlag);
+    }
+
+    public void doKitchenWork() {
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishWasher.doDishes();
+    }
+
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+}
+
+
+class CoffeeMaker {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee() {
+        if (hasWorkToDo) {
+            System.out.println("Coffee is brewing...");
+            hasWorkToDo = false;
+        } else {
+            System.out.println("No work to do.");
+        }
+    }
+
+}
+
+class Refrigerator {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood() {
+        if (hasWorkToDo) {
+            System.out.println("Ordering food...");
+            hasWorkToDo = false;
+        } else {
+            System.out.println("No work to do.");
+        }
+    }
+
+}
+
+class DishWasher {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes() {
+        if (hasWorkToDo) {
+            System.out.println("Dishes are being washed...");
+            hasWorkToDo = false;
+        } else {
+            System.out.println("No work to do.");
+        }
+    }
+
+}
